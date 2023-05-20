1667f0a4baaada4e03667baa33ae58e727764600
Shisha <shishkarev16@gmail.com>
2023-05-20 15:40:45 +0300

    +buttonComp

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -6,6 +6,7 @@
       <Panel label="Всего отзывов" icon="chat" views="10000+" />
       <Panel label="Всего партнеров" icon="parther" views="25" />
     </div>
+    <DownloadButton label="Импорт в .xls" icon="document"/>
     <div class="flex justify-center p-10">
       <Map />
     </div>
@@ -17,12 +18,14 @@ import TheHeader from "@/components/TheHeader.vue";
 import TheFooter from "@/components/TheFooter.vue";
 import Panel from "@/components/Panel.vue";
 import Map from "@/components/Map.vue";
+import DownloadButton from "@/components/DownloadButton.vue";
 export default {
   components: {
     TheFooter,
     TheHeader,
     Panel,
     Map,
+    DownloadButton
   },
 };
 </script>
