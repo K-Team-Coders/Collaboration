e8ed4bcdbc986e9d32081f60d99d92dc69448147
Your Carma <y0urcarma@yandex.ru>
2023-05-20 15:53:17 +0300

    MapisWorking

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -3,7 +3,7 @@
     <TheHeader />
     <Panel />
     <div class="flex justify-center p-10">
-      <Map />
+      <Map :postamat_list="allpostamats"/>
     </div>
   </div>
 </template>
@@ -12,6 +12,7 @@
 import TheHeader from "@/components/TheHeader.vue";
 import TheFooter from "@/components/TheFooter.vue";
 import Panel from "@/components/Panel.vue";
+import { mapActions, mapGetters } from "vuex";
 import Map from "@/components/Map.vue";
 export default {
   components: {
@@ -20,5 +21,32 @@ export default {
     Panel,
     Map,
   },
+  data () {
+    return{
+      id: 2,
+        usertext: 'Все классно',
+        grade: 5,
+        adress: '',
+        reviewdate: '2023-05-20T11:07:15.590Z',
+        clusername: 1,
+        article: 'Пиво',
+        seller: 'БТК-Групп',
+        latitude: 0,
+        longtitude: 0,
+    }
+  },
+  computed: {
+    ...mapGetters(["allpostamats"]),
+  },
+  methods: {
+    ...mapActions([
+      "GET_ALLPOSTAMATS",
+    ])
+
+  },
+  async created(){
+    this.GET_ALLPOSTAMATS()
+
+  }
 };
 </script>
