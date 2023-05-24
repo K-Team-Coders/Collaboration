c90c1c133fcb3441720639df9bd8746299a07ea2
Your Carma <y0urcarma@yandex.ru>
2023-05-24 21:11:18 +0300

    Again

--- frontend/src/components/charts/AreaChartApex.vue
+++ frontend/src/components/charts/AreaChartApex.vue
@@ -1,53 +1,170 @@
 <template>
-    <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+  
+  <div id="chart">
+        <div class="toolbar">
+          <button id="one_month"
+              @click="updateData('one_month')" :class="{active: selection==='one_month'}"
+              >
+            1M
+          </button>
+          
+          <button id="six_months"
+              @click="updateData('six_months')" :class="{active: selection==='six_months'}"
+              >
+            6M
+          </button>
+          
+          <button id="one_year"
+              
+              @click="updateData('one_year')" :class="{active: selection==='one_year'}"
+              >
+            1Y
+          </button>
+          
+          <button id="ytd"
+              @click="updateData('ytd')" :class="{active: selection==='ytd'}"
+              >
+            YTD
+          </button>
+          
+          <button id="all"
+              @click="updateData('all')" :class="{active: selection==='all'}"
+              >
+            ALL
+          </button>
+        </div>
+      
+        <div id="chart-timeline">
+        <apexchart type="area" height="350" ref="chart" :options="chartOptions" :series="series"></apexchart>
+      </div>
+      </div>
 </template>
 
 <script>
+ import VueApexCharts from "vue3-apexcharts";
 export default {
-    data: {
+  props:{
+data_chart: Object
+  },
+  components: {
+          apexchart: VueApexCharts,
+        },
+  data(){
+    return{
           
           series: [{
-            name: "STOCK ABC",
-            data: series.monthDataSeries1.prices
+            data: [
+              this.data
+            ]
           }],
           chartOptions: {
             chart: {
+              id: 'area-datetime',
               type: 'area',
               height: 350,
               zoom: {
-                enabled: false
+                autoScaleYaxis: true
               }
+            },
+            annotations: {
+              yaxis: [{
+                y: 30,
+                borderColor: '#999',
+                label: {
+                  show: true,
+                  text: 'Support',
+                  style: {
+                    color: "#fff",
+                    background: '#00E396'
+                  }
+                }
+              }],
+              xaxis: [{
+                x: new Date('14 Nov 2012').getTime(),
+                borderColor: '#999',
+                yAxisIndex: 0,
+                label: {
+                  show: true,
+                  text: 'Rally',
+                  style: {
+                    color: "#fff",
+                    background: '#775DD0'
+                  }
+                }
+              }]
             },
             dataLabels: {
               enabled: false
             },
-            stroke: {
-              curve: 'straight'
+            markers: {
+              size: 0,
+              style: 'hollow',
             },
-            
-            title: {
-              text: 'Fundamental Analysis of Stocks',
-              align: 'left'
-            },
-            subtitle: {
-              text: 'Price Movements',
-              align: 'left'
-            },
-            labels: series.monthDataSeries1.dates,
             xaxis: {
               type: 'datetime',
+              min: new Date('01 Mar 2012').getTime(),
+              tickAmount: 6,
             },
-            yaxis: {
-              opposite: true
+            tooltip: {
+              x: {
+                format: 'dd MMM yyyy'
+              }
             },
-            legend: {
-              horizontalAlign: 'left'
-            }
+            fill: {
+              type: 'gradient',
+              gradient: {
+                shadeIntensity: 1,
+                opacityFrom: 0.7,
+                opacityTo: 0.9,
+                stops: [0, 100]
+              }
+            },
           },
           
-          
-        },
+          selection: 'one_year',
+        }},
+        methods: {
+          updateData: function(timeline) {
+            this.selection = timeline
+        
+            switch (timeline) {
+              case 'one_month':
+                this.$refs.chart.zoomX(
+                  new Date('28 Jan 2013').getTime(),
+                  new Date('27 Feb 2013').getTime()
+                )
+                break
+              case 'six_months':
+                this.$refs.chart.zoomX(
+                  new Date('27 Sep 2012').getTime(),
+                  new Date('27 Feb 2013').getTime()
+                )
+                break
+              case 'one_year':
+                this.$refs.chart.zoomX(
+                  new Date('27 Feb 2012').getTime(),
+                  new Date('27 Feb 2013').getTime()
+                )
+                break
+              case 'ytd':
+                this.$refs.chart.zoomX(
+                  new Date('01 Jan 2013').getTime(),
+                  new Date('27 Feb 2013').getTime()
+                )
+                break
+              case 'all':
+                this.$refs.chart.zoomX(
+                  new Date('23 Jan 2012').getTime(),
+                  new Date('27 Feb 2013').getTime()
+                )
+                break
+              default:
+            }
+          }
         }
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
