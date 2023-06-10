902044603dd40631d451dd0f4f46cbc52351d884
gosha-1234 <71758216+gosha-1234@users.noreply.github.com>
2023-06-11 00:33:44 +0300

    Update commands_help_and_company.py

--- services/telegrambot/commands_help_and_company.py
+++ services/telegrambot/commands_help_and_company.py
@@ -30,7 +30,13 @@ def sendReview(mark, usertext, classnumber):
                  }
              )
 
-
+@bot.message_handler(commands=['company'])
+def information_company(info_company):
+    bot.send_message(info_company.chat.id, 'Проект «Московский постамат» стал ответом на запрос москвичей '
+                                           'в бесконтактной, быстрой и удобной системе доставки, расположенной '
+                                           'максимально близко к жителям. Сейчас проект находится в стадии реализации '
+                                           'и в будущем нацелен на создание в Москве сети, '
+                                           'включающей более 10 000 постаматов.')
 
 @bot.message_handler(commands=['help'])
 def help_command(cmd_help):
@@ -50,58 +56,65 @@ def help_command(cmd_help):
 @bot.callback_query_handler(func=lambda call_help: True)
 def callback_help(call_help):
     if call_help.data == '11':
+
         bot.send_message(call_help.from_user.id, "Приносим свои извинения, за доставленные неудобства 🥺 "
                                                  "Подробно опишите возникшую проблему с товаром")
+        print(call_help)
+        @bot.message_handler(content_types=['text'])
+        def reply_message(rpl_msg):
+            bot.reply_to(rpl_msg, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
+            mark = 1.0
+            class_number = 1
 
-        bot.reply_to(call_help, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
-        mark = 1.0
-        class_number = 1
-        text_help = call_help.text
-        sendReview(mark, usertext=text_help, classnumber=class_number)
+            text_help = rpl_msg.text
+            sendReview(mark, usertext=text_help, classnumber=class_number)
 
     elif call_help.data == '12':
         bot.send_message(call_help.from_user.id, "Приносим свои извинения, за доставленные неудобства 🥺 "
                                                  "Подробно опишите возникшую проблему с курьером")
-        bot.reply_to(call_help, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
-        mark = 1.0
-        class_number = 2
-        text_help = call_help.text
-        sendReview(mark, usertext=text_help, classnumber=class_number)
+
+        @bot.message_handler(content_types=['text'])
+        def reply_message(rpl_msg_two):
+            bot.reply_to(rpl_msg_two, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
+            mark = 1.0
+            class_number = 2
+            text_help = rpl_msg_two.text
+            sendReview(mark, usertext=text_help, classnumber=class_number)
 
     elif call_help.data == '13':
         bot.send_message(call_help.from_user.id, "Приносим свои извинения, за доставленные неудобства 🥺 "
                                                  "Подробно опишите возникшую проблему с постаматом")
-        bot.reply_to(call_help, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
-        mark = 1.0
-        class_number = 3
-        text_help = call_help.text
-        sendReview(mark, usertext=text_help, classnumber=class_number)
+
+        @bot.message_handler(content_types=['text'])
+        def reply_message(rpl_msg_three):
+            bot.reply_to(rpl_msg_three, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
+            mark = 1.0
+            class_number = 3
+            text_help = rpl_msg_three.text
+            sendReview(mark, usertext=text_help, classnumber=class_number)
 
     elif call_help.data == '14':
         bot.send_message(call_help.from_user.id, "Приносим свои извинения, за доставленные неудобства 🥺 "
                                                  "Подробно опишите возникшую проблему")
-        bot.reply_to(call_help, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
-        mark = 1.0
-        class_number = 4
-        text_help = call_help.text
-        sendReview(mark, usertext=text_help, classnumber=class_number)
+
+        @bot.message_handler(content_types=['text'])
+        def reply_message(rpl_msg_four):
+            bot.reply_to(rpl_msg_four, "Я передал Вашу жалобу специалистам, в скором времени это исправят")
+            mark = 1.0
+            class_number = 4
+            text_help = rpl_msg_four.text
+            sendReview(mark, usertext=text_help, classnumber=class_number)
 
     elif call_help.data == '15':
         bot.send_message(call_help.from_user.id, "Мы рады, когда у наших клиентов все замечательно."
-                                                 " Спасибо, что Вы выбрали именно нас, до скорых встреч👋🏻")
+                                                 "Спасибо, что Вы выбрали именно нас, до скорых встреч👋🏻")
         mark = 5
         class_number_from_15 = 0
         sendReview(mark, usertext='Все хорошо', classnumber=class_number_from_15)
 
     bot.answer_callback_query(callback_query_id=call_help.id)
 
-@bot.message_handler(commands=['company'])
-def information_company(info_company):
-    bot.send_message(info_company.chat.id, 'Проект «Московский постамат» стал ответом на запрос москвичей '
-                                           'в бесконтактной, быстрой и удобной системе доставки, расположенной '
-                                           'максимально близко к жителям. Сейчас проект находится в стадии реализации '
-                                           'и в будущем нацелен на создание в Москве сети, '
-                                           'включающей более 10 000 постаматов.')
+
 
 
 bot.polling(none_stop=True, interval=0)
